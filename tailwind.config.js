411bad9e30395b46b0de01f1c94090d5c7f6b18f
João Vitor <joaovitoraguiar.priv@gmail.com>
2024-07-30 00:10:18 -0300

    installing maryui

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,17 @@ import typo from '@tailwindcss/typography'
 
 export default {
   content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-  ],
+		"./resources/**/*.blade.php",
+		 "./resources/**/*.js",
+		 "./vendor/robsontenorio/mary/src/View/Components/**/*.php"
+	],
   theme: {
     extend: {},
   },
   plugins: [
-    forms,
-    typo
-  ],
+		forms,
+		typo,
+		require("daisyui")
+	],
 }
 
